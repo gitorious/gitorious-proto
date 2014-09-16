6431b15f51fbc91745488dd3a4771ce27b6676ed
Marcin Kulik <m@ku1ik.com>
2014-09-16 13:41:42 +0200

    Set optional repo config attributes as GITORIOUS_* env vars

--- common/env.go
+++ common/env.go
@@ -12,9 +12,29 @@ func CreateEnv(protocol, username string, repoConfig *api.RepoConfig) []string {
 	env = append(env, "GITORIOUS_PROTO="+protocol)
 	env = append(env, "GITORIOUS_USER="+username) // used by hooks
 
-	// if repoConfig.CustomPreReceivePath != "" {
-	// 	env = append(env, "GITORIOUS_CUSTOM_PRE_RECEIVE=1")
-	// }
+	if repoConfig.SshCloneUrl != "" {
+		env = append(env, "GITORIOUS_SSH_CLONE_URL="+repoConfig.SshCloneUrl)
+	}
+
+	if repoConfig.HttpCloneUrl != "" {
+		env = append(env, "GITORIOUS_HTTP_CLONE_URL="+repoConfig.HttpCloneUrl)
+	}
+
+	if repoConfig.GitCloneUrl != "" {
+		env = append(env, "GITORIOUS_GIT_CLONE_URL="+repoConfig.GitCloneUrl)
+	}
+
+	if repoConfig.CustomPreReceivePath != "" {
+		env = append(env, "GITORIOUS_CUSTOM_PRE_RECEIVE_PATH="+repoConfig.CustomPreReceivePath)
+	}
+
+	if repoConfig.CustomPostReceivePath != "" {
+		env = append(env, "GITORIOUS_CUSTOM_POST_RECEIVE_PATH="+repoConfig.CustomPostReceivePath)
+	}
+
+	if repoConfig.CustomUpdatePath != "" {
+		env = append(env, "GITORIOUS_CUSTOM_UPDATE_PATH="+repoConfig.CustomUpdatePath)
+	}
 
 	return env
 }
---
+++ common/env_test.go
@@ -0,0 +1,70 @@
+package common
+
+import (
+	"os"
+	"strings"
+	"testing"
+
+	"gitorious.org/gitorious/gitorious-proto/api"
+)
+
+func assertPresence(env []string, expected string, t *testing.T) {
+	for _, envVar := range env {
+		if envVar == expected {
+			return
+		}
+	}
+
+	t.Errorf("expected %v in env but not found", expected)
+}
+
+func assertAbsence(env []string, notExpected string, t *testing.T) {
+	for _, envVar := range env {
+		pair := strings.Split(envVar, "=")
+		if pair[0] == notExpected {
+			t.Errorf("not expected %v in env but found", notExpected)
+			return
+		}
+	}
+}
+
+func TestCreateEnv(t *testing.T) {
+	repoConfig := &api.RepoConfig{}
+
+	env := CreateEnv("ssh", "sickill", repoConfig)
+
+	// make sure it is based on the existing environment
+	assertPresence(env, "HOME="+os.Getenv("HOME"), t)
+
+	// ensure required vars are set
+	assertPresence(env, "GITORIOUS_PROTO=ssh", t)
+	assertPresence(env, "GITORIOUS_USER=sickill", t)
+
+	// ensure optional vars are not set
+	assertAbsence(env, "GITORIOUS_SSH_CLONE_URL", t)
+	assertAbsence(env, "GITORIOUS_HTTP_CLONE_URL", t)
+	assertAbsence(env, "GITORIOUS_GIT_CLONE_URL", t)
+	assertAbsence(env, "GITORIOUS_CUSTOM_PRE_RECEIVE_PATH", t)
+	assertAbsence(env, "GITORIOUS_CUSTOM_POST_RECEIVE_PATH", t)
+	assertAbsence(env, "GITORIOUS_CUSTOM_UPDATE_PATH", t)
+
+	repoConfig = &api.RepoConfig{
+		SshCloneUrl:  "ssh-clone-url",
+		HttpCloneUrl: "http-clone-url",
+		GitCloneUrl:  "git-clone-url",
+
+		CustomPreReceivePath:  "custom-pre-receive",
+		CustomPostReceivePath: "custom-post-receive",
+		CustomUpdatePath:      "custom-update",
+	}
+
+	env = CreateEnv("ssh", "sickill", repoConfig)
+
+	// ensure optional vars are not set
+	assertPresence(env, "GITORIOUS_SSH_CLONE_URL=ssh-clone-url", t)
+	assertPresence(env, "GITORIOUS_HTTP_CLONE_URL=http-clone-url", t)
+	assertPresence(env, "GITORIOUS_GIT_CLONE_URL=git-clone-url", t)
+	assertPresence(env, "GITORIOUS_CUSTOM_PRE_RECEIVE_PATH=custom-pre-receive", t)
+	assertPresence(env, "GITORIOUS_CUSTOM_POST_RECEIVE_PATH=custom-post-receive", t)
+	assertPresence(env, "GITORIOUS_CUSTOM_UPDATE_PATH=custom-update", t)
+}
